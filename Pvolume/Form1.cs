73833c41cba8ff162c480c1f4b41d3b49a6b54d1
Ruan-Bertholino <160425110+Ruan-Bertholino@users.noreply.github.com>
2024-03-12 14:22:21 -0300

    Add files via upload

---
+++ Pvolume/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pvolume
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtNome_Validated(object sender, EventArgs e)
+        {
+            double TesteRaio;
+            if (!double.TryParse(txtRaio.Text, out TesteRaio))
+            {
+                MessageBox.Show("Raio Invalido");
+            }
+        }
+
+        private void txtAltura_Validated(object sender, EventArgs e)
+        {
+            double TesteAltura;
+            if (!double.TryParse(txtAltura.Text, out TesteAltura))
+            {
+                MessageBox.Show("Altura Invalida");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double TesteRaio, TesteAltura;
+            if ((!double.TryParse(txtRaio.Text, out TesteRaio)) || (!double.TryParse(txtAltura.Text, out TesteAltura)))
+            {
+                MessageBox.Show("Valores Invalidos");
+                txtRaio.Focus();
+            }
+            else
+            {
+                double volume;
+                volume = Math.PI * Math.Pow(TesteRaio, 2) * TesteAltura;
+                txtVolume.Text = volume.ToString("N2");
+            }
+        }
+    }
+}
